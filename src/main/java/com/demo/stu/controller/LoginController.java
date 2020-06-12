cf3e0144a960cf84f6ebe5eb5a7958afd23931e5
chenjian <hi.cj@outlook.com>
2020-06-12 16:20:37 +0800

    feat(登录): 新增登录界面

--- src/main/java/com/demo/stu/controller/LoginController.java
+++ src/main/java/com/demo/stu/controller/LoginController.java
@@ -1,8 +1,13 @@
 package com.demo.stu.controller;
 
+import com.demo.stu.entity.enumcode.UserType;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -13,10 +18,22 @@ public class LoginController {
 
     Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-    @RequestMapping("login")
+    @GetMapping("login")
     public String login() {
-        logger.info("login");
-        return "loginView";
+        return "login";
+    }
+
+    @PostMapping("login")
+    public String loginAction(String username, String password, UserType type, Model model) {
+        model.addAttribute("username", username);
+        assert null != type;
+        if (StringUtils.isAnyEmpty(username, password)) {
+            model.addAttribute("error", "请输入用户名/密码");
+        }
+        if (type == UserType.STUDENT) {
+
+        }
+        return "login";
     }
 
 }
