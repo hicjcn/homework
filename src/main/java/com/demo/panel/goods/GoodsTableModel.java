ac32094676530b6225fcc86eb45ac4d004e54b4d
chenjian <hi.cj@outlook.com>
2020-06-16 18:33:35 +0800

    表头显示

--- src/main/java/com/demo/panel/goods/GoodsTableModel.java
+++ src/main/java/com/demo/panel/goods/GoodsTableModel.java
@@ -11,15 +11,6 @@ public class GoodsTableModel extends AbstractTableModel {
 
     private Object[][] data = null;
 
-    /**
-     * Returns the number of rows in the model. A
-     * <code>JTable</code> uses this method to determine how many rows it
-     * should display.  This method should be quick, as it
-     * is called frequently during rendering.
-     *
-     * @return the number of rows in the model
-     * @see #getColumnCount
-     */
     @Override
     public int getRowCount() {
         if (null == data) {
@@ -28,33 +19,22 @@ public class GoodsTableModel extends AbstractTableModel {
         return data.length;
     }
 
-    /**
-     * Returns the number of columns in the model. A
-     * <code>JTable</code> uses this method to determine how many columns it
-     * should create and display by default.
-     *
-     * @return the number of columns in the model
-     * @see #getRowCount
-     */
     @Override
     public int getColumnCount() {
         return columnNames.length;
     }
 
-    /**
-     * Returns the value for the cell at <code>columnIndex</code> and
-     * <code>rowIndex</code>.
-     *
-     * @param rowIndex    the row whose value is to be queried
-     * @param columnIndex the column whose value is to be queried
-     * @return the value Object at the specified cell
-     */
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         if (null == data) {
             return null;
         }
         return data[rowIndex][columnIndex];
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columnNames[column].toString();
     }
 
     /**
