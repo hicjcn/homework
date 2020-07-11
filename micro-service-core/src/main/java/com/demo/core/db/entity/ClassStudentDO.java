d0b0444b67f6f3d4bce364f853db9ababd828890
czc <877100446@qq.com>
2020-07-11 18:50:22 +0800

    msg

--- micro-service-core/src/main/java/com/demo/core/db/entity/ClassStudentDO.java
+++ micro-service-core/src/main/java/com/demo/core/db/entity/ClassStudentDO.java
@@ -1,5 +1,7 @@
 package com.demo.core.db.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import lombok.Data;
@@ -23,6 +25,7 @@ public class ClassStudentDO implements Serializable {
     /**
      * 关联ID
      */
+    @TableId(value = "cs_id", type = IdType.AUTO)
     private Integer csId;
 
     /**
