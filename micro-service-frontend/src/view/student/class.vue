e1c01659bc56abb8cfe109796787c088b264c210
chenjian <hi.cj@outlook.com>
2020-07-11 15:04:49 +0800

    feat: 学生申请加入班级

--- micro-service-frontend/src/view/student/class.vue
+++ micro-service-frontend/src/view/student/class.vue
@@ -7,34 +7,28 @@
                 style="width: 100%">
             <el-table-column
                     fixed
-                    prop="date"
-                    label="日期"
+                    prop="classId"
+                    label="编号"
                     width="150">
             </el-table-column>
             <el-table-column
-                    prop="name"
-                    label="姓名"
+                    prop="className"
+                    label="名称"
                     width="120">
             </el-table-column>
             <el-table-column
-                    prop="province"
-                    label="省份"
+                    prop="teacherCode"
+                    label="教师编号"
                     width="120">
             </el-table-column>
             <el-table-column
-                    prop="city"
-                    label="市区"
+                    prop="teacherName"
+                    label="教师"
                     width="120">
             </el-table-column>
             <el-table-column
-                    prop="address"
-                    label="地址"
-                    width="300">
-            </el-table-column>
-            <el-table-column
-                    prop="zip"
-                    label="邮编"
-                    width="120">
+                    prop="classDescribe"
+                    label="班级描述">
             </el-table-column>
             <el-table-column
                     fixed="right"
@@ -49,46 +43,43 @@
 </template>
 
 <script>
+    import request from "@/util/request";
+
     export default {
         name: "class",
         methods: {
+            refreshData() {
+                request.get('/class/getStudentClassList', null)
+                    .then(res => {
+                        if (res.code === '200') {
+                            this.tableData = res.data
+                        }
+                    })
+            },
+
             handleClick(row) {
                 console.log(row);
+                request.post('/classStudent/applyForClass', {
+                    classId: row.classId
+                }).then(res => {
+                    if (res.code === '200') {
+                        this.$message({
+                            message: '申请成功，等待教师审核',
+                            type: 'success'
+                        });
+                    }
+                })
             }
         },
 
         data() {
             return {
-                tableData: [{
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1517 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1519 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1516 弄',
-                    zip: 200333
-                }]
+                tableData: []
             }
+        },
+
+        created() {
+            this.refreshData()
         }
     }
 </script>
